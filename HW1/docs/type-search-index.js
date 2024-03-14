fd8654cf226e4cc7ad48848d7878e10f18060e55
Nikolai Tikhomirov <79159489230@yandex.ru>
2024-03-14 23:21:25 +0300

    javadoc added

---
+++ HW1/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Note"},{"p":"org.example","l":"Writer"}];updateSearchResults();
